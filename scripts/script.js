f7a666303b6e646cdddad1c0a03cc9a21235c2ad
Jonathan Hall <143550170+Jonathan-Hall34@users.noreply.github.com>
2024-11-06 20:59:41 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,6 @@
 $("button").click(function(){
     $.getJSON("https://Jonathan-Hall34.github.io/nba-scores-json/data.json", function(data){
-        
         $("div").empty();
-    
         $("div").append(data.message + " " + data.url);
     });
 });
